e07417994fc360599e3c54c4df57e07b5fd3ea20
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-05-18 15:21:12 -0300

    nit

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -10,7 +10,9 @@ resource "azurerm_subnet" "default" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefixes     = ["10.0.0.0/24"]
-  service_endpoints    = ["Microsoft.Storage"]
+
+  # Delegation is not required
+  service_endpoints = ["Microsoft.Storage"]
 }
 
 resource "azurerm_subnet" "private_endpoints" {
